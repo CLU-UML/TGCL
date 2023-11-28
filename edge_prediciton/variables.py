384f3b2696a0d1a6f44a5bb5b255f54e87d9367e
nidhi <nvakil@cs.uml.edu>
2023-11-28 16:29:06 -0500

    created folders

--- edge_prediciton/variables.py
+++ edge_prediciton/variables.py
@@ -2,7 +2,7 @@ import torch
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-dir_model = "../spaced_repetation_models"
+dir_model = "../saved_models"
 dir_logs = "../logs"
 dir_data = "../data"
 
---
+++ saved_models/readme.txt
@@ -0,0 +1,1 @@
+This repository will save trained models.
